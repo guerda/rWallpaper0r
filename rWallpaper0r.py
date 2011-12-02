8fa3c1545de42d3c86f618491bd4375f75db8691
Philip Gillißen <guerda@freenet.de>
2011-12-02 22:13:15 +0100

    Modified wallpaper_url_fetcher.py for more comfort
    saves the file now with correct mimetype

---
+++ rWallpaper0r.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+from wallpaper_url_fetcher import save_new_wallpaper
+import gconf
+
--- wallpaper_url_fetcher.py
+++ wallpaper_url_fetcher.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import urllib2
 import json
+import mimetypes
 
 reddit_url='http://www.reddit.com/r/wallpaper/top/.json?sort=top&t=day'
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'rWallpaper0r/0.1')]
+mimetypes.init()
+
 
 def fetch_new_wallpaper_url():
 	url = opener.open(reddit_url)
@@ -12,14 +15,28 @@ def fetch_new_wallpaper_url():
 	for line in url:
 		data = data + line + "\n"
 	json_data = json.loads(data)
-	return json_data["data"]["children"][0]["data"]["url"]
+	first_entry = json_data["data"]["children"][0]["data"]
+	wallpaper_url = first_entry["url"]
+	wallpaper_title = first_entry["title"]
+	return [wallpaper_url, wallpaper_title]
 
 def fetch_new_wallpaper_data():
-	url = opener.open(fetch_new_wallpaper_url())
+	url = opener.open(fetch_new_wallpaper_url()[0])
 	data = ''
 	for line in url:
 		data = data + line
 	return data
 
+def save_new_wallpaper():
+	wallpaper_data = fetch_new_wallpaper_url()
+	data = opener.open(wallpaper_data[0])
+	mime_type = data.info().gettype()
+	filename = str(wallpaper_data[1]).replace(" ", "_").replace("\"", "").replace("'","")
+	filename = "%s%s" % (filename, mimetypes.guess_extension(mime_type))
+	local_file = open(filename, "wb")
+	local_file.write(data.read())
+	local_file.close()
+	return filename
+
 if __name__ == "__main__":
-	print(fetch_new_wallpaper_data())
+	print(save_new_wallpaper())
