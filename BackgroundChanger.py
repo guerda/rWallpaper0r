0ef5c7c45ea35949d4217d21da25e763b1f78174
Philip Gillißen <guerda@freenet.de>
2011-12-11 18:16:04 +0100

    +First version of BackgroundChanging mechanism for unity/Gnome + gitignore

---
+++ BackgroundChanger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from gi.repository import Gtk, Gio
+
+class BackgroundChanger():
+	SCHEMA = 'org.gnome.desktop.background'
+	KEY = 'picture-uri'
+	
+	def change_background(self, filename):
+		gsettings = Gio.Settings.new(self.SCHEMA)
+		print(gsettings.get_string(self.KEY))
+		print(gsettings.set_string(self.KEY, "file://" + filename))
+		print(gsettings.get_string(self.KEY))
+
+if __name__ == "__main__":
+	BackgroundChanger().change_background("/home/philip/Entwicklung/Python/rWallpaper0r/Eyes.jpg")
