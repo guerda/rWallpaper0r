e3b2d001f8c05c4004313d14f93b29ec71cb91c3
Philip Gillißen <guerda@freenet.de>
2012-03-11 18:16:37 +0100

    Refactoring to PEP-8

--- wallpaperfetcher.py
+++ wallpaperfetcher.py
@@ -7,17 +7,18 @@ from os import path
 import os
 
 class WallpaperFetcher():
-    WALLPAPER_DIR=("%s/%s")%(path.expanduser("~"),'Bilder/rWallpaper0r/')
-    # check, if wallpaper dir exists. If not, create it.
-    if not os.path.exists(WALLPAPER_DIR):
-        os.makedirs(WALLPAPER_DIR)
-    reddit_url='http://www.reddit.com/r/wallpapers/top/.json?sort=top&t=day'
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', 'rWallpaper0r/0.1')]
-    mimetypes.init()
+    def __init__(self):
+        self.WALLPAPER_DIR=("%s/%s")%(path.expanduser("~"),'Bilder/rWallpaper0r/')
+        # check, if wallpaper dir exists. If not, create it.
+        if not os.path.exists(self.WALLPAPER_DIR):
+            os.makedirs(self.WALLPAPER_DIR)
+        self.reddit_url='http://www.reddit.com/r/wallpapers/top/.json?sort=top&t=day'
+        self.opener = urllib.request.build_opener()
+        self.opener.addheaders = [('User-agent', 'rWallpaper0r/0.1')]
+        mimetypes.init()
 
     def fetch_new_wallpaper_url(self):
-        url = opener.open(reddit_url)
+        url = self.opener.open(self.reddit_url)
         data_bytes = url.read()
         json_string = str(data_bytes, "UTF-8")
         json_data = json.loads(json_string)
@@ -26,7 +27,8 @@ class WallpaperFetcher():
         wallpaper_url = entry["url"]
         wallpaper_title = entry["title"]
         print(wallpaper_url[-3:])
-        while wallpaper_url[-3:] != "jpg" and wallpaper_url[-4:] != "jpeg" and wallpaper_url[-3:] != "png":
+        while wallpaper_url[-3:] != "jpg" and wallpaper_url[-4:] != "jpeg" \
+                and wallpaper_url[-3:] != "png":
             entry = json_data["data"]["children"][i]["data"]
             wallpaper_url = entry["url"]
             wallpaper_title = entry["title"]
@@ -35,28 +37,31 @@ class WallpaperFetcher():
         return [wallpaper_url, wallpaper_title]
 
     def fetch_new_wallpaper_data(self):
-        url = opener.open(fetch_new_wallpaper_url()[0])
+        url = self.opener.open(self.fetch_new_wallpaper_url()[0])
         data = ''
         for line in url:
 	        data = data + line
         return data
 
     def save_new_wallpaper(self):
-        wallpaper_data = fetch_new_wallpaper_url()
-        data = opener.open(wallpaper_data[0])
+        wallpaper_data = self.fetch_new_wallpaper_url()
+        data = self.opener.open(wallpaper_data[0])
         filename = str(wallpaper_data[1])
         mime_type = data.info().get_content_maintype()
-        filename = filename.replace(" ", "_").replace("\"", "").replace("'","").replace(",","").replace(".","_").replace(":","_").replace("?","_")
+        filename = filename.replace(" ", "_").replace("\"", "")
+        filename = filename.replace("'","").replace(",","").replace(".","_")
+        filename = filename.replace(":","_").replace("?","_")
         extension = mimetypes.guess_extension(mime_type)
         if(extension == ".jpe"):
             extension = ".jpg"
         elif(extension == None):
             extension = ""
-        filename = "%s%s%s" % (WALLPAPER_DIR, filename, extension) 
+        filename = "%s%s%s" % (self.WALLPAPER_DIR, filename, extension) 
         local_file = open(filename, "wb")
         local_file.write(data.read())
         local_file.close()
         return path.abspath(filename)
 
 if __name__ == "__main__":
-    print(save_new_wallpaper())
+    fetcher = WallpaperFetcher()
+    print(fetcher.save_new_wallpaper())
